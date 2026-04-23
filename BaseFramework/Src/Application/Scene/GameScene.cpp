521260fa4cd01767337f318da31630a78b22d55b
kd1202015 <kd1413000@st.kobedenshi.ac.jp>
2026-04-23 10:14:45 +0900

    代码复制上传测试

---
+++ BaseFramework/Src/Application/Scene/GameScene.cpp
@@ -0,0 +1,26 @@
+﻿#include "GameScene.h"
+
+void GameScene::PreUpate()
+{	
+	KdDebugGUI::Instance().AddLog("PreUpdate()\n");
+}
+
+void GameScene::Upate()
+{
+	KdDebugGUI::Instance().AddLog("Upate()\n");
+}
+
+void GameScene::Draw()
+{
+	KdDebugGUI::Instance().AddLog("Draw()\n");
+}
+
+void GameScene::Init()
+{
+	KdDebugGUI::Instance().AddLog("Init()\n");
+}
+
+void GameScene::Release()
+{
+	KdDebugGUI::Instance().AddLog("Release()\n");
+}
---
+++ BaseFramework/Src/Application/Scene/GameScene.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class GameScene
+{
+	//public: 哪里都能akuess
+public:
+	GameScene(){}//生成时调用
+	~GameScene() {}//销毁时调用
+
+	void PreUpate();//更新前处理
+	void Upate();//更新
+	void Draw();//绘制
+	void Init();//初始化
+	void Release();//释放 解放处理
+	//private: 只有类内能访问
+
+private:
+	int test = 0;
+};
